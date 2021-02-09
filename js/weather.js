a64405229641509286dc66713ad26950308b787e
asher.dev <grit.developer.asher@gmail.com>
2021-02-09 16:50:33 +0900

    Add current weather through the Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,22 @@
+const weatherApiKey = 'f71b1c8916611cf4db595cc59f68bf20';
+const textCoords = 'coords';
+const weather = document.querySelector('.weather');
+
+function getWeahter(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=metric`)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        const temp = json.main.temp;
+        const place = json.name;
+        
+        weather.innerText = `${temp}℃ / ${place}`;
+    });
+}
+
 function saveCoords(obj) {
-    localStorage.setItem("coords", JSON.stringify(obj));
+    localStorage.setItem(textCoords, JSON.stringify(obj));
 }
 
 function handleGeoSucces(position) {
@@ -22,12 +39,14 @@ function askForCoords() {
 }
 
 function loadCoords() {
-    const loadedCoords = localStorage.getItem("coords");
+    const loadedCoords = localStorage.getItem(textCoords);
 
     if(loadedCoords === null) {
         askForCoords();
     } else {
-        //getWeahter
+        const parsedCoords = JSON.parse(loadedCoords);
+        // console.log(parsedCoords);
+        getWeahter(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
